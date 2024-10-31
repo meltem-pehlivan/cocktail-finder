1a54b643278d95b3e424e0ce48c26c758ea04706
Meltem Pehlivan <meltem-pehlivan@hotmail.com>
2024-11-01 00:01:35 +0300

    background edited

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CocktailInput from './CocktailInput';
 function App() {
 
   return (
-       <div>
+       <div className = "App">
        <h1>COCKTAIL & MORE</h1>
        <p>Mükemmel Tarifler İçin Elindeki Malzemeleri Gir!</p>
        <CocktailInput/>
